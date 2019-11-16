d8a87ad9abd0d9d393dd90942c0b153af0b142a7
Paul Bailey <paul.m.bailey@gmail.com>
2019-11-16 17:24:46 -0600

    admin setup

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,29 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.postgres.forms import SplitArrayField
 
-# Register your models here.
+from polls.models import Poll, Vote
+
+class VoteInline(admin.TabularInline):
+  model = Vote
+  raw_id_fields = ('user',)
+
+
+class PollForm(forms.ModelForm):
+  choices = SplitArrayField(
+    forms.CharField(required=False, max_length=254), 8, remove_trailing_nulls=True)
+
+  class Meta:
+    model = Poll
+    fields = '__all__'
+
+  class Media:
+    css = {'screen': ("admin/poll.css",)}
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+  list_display = ('question', 'slug', 'published', 'ended')
+  raw_id_fields = ('next_poll',)
+  inlines = [VoteInline]
+  form = PollForm
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Poll(models.Model):
+  question = models.CharField(max_length=254)
+  slug = models.SlugField(max_length=254)
+  choices = ArrayField(models.CharField(max_length=254))
+
+  published = models.BooleanField(default=False)
+  ended = models.BooleanField(default=False)
+
+  next_poll = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+  created = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ('-created',)
+
+  def __str__(self):
+    return self.question
+
+
+class Vote(models.Model):
+  poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  choice = models.PositiveSmallIntegerField()
+
+  def __str__(self):
+    return f'{self.poll} - {self.user}'
