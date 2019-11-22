9bc71b490a126aee109f3d68b0d24f540501b3ff
Paul Bailey <paul.m.bailey@gmail.com>
2019-11-22 07:34:45 -0800

    presentation model

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.contrib.postgres.forms import SplitArrayField
 
-from polls.models import Poll, Vote
+from polls.models import Presentation, Poll, Vote
 
 class VoteInline(admin.TabularInline):
   model = Vote
@@ -20,10 +20,17 @@ class PollForm(forms.ModelForm):
   class Media:
     css = {'screen': ("admin/poll.css",)}
 
+class PollInline(admin.TabularInline):
+  model = Poll
+
+@admin.register(Presentation)
+class PresentationAdmin(admin.ModelAdmin):
+  list_display = ('title', 'slug')
+  inlines = [PollInline]
 
 @admin.register(Poll)
 class PollAdmin(admin.ModelAdmin):
-  list_display = ('question', 'slug', 'published', 'ended')
-  raw_id_fields = ('next_poll',)
+  list_display = ('question', 'published', 'ended')
+  raw_id_fields = ('presentation',)
   inlines = [VoteInline]
   form = PollForm
--- polls/models.py
+++ polls/models.py
@@ -2,17 +2,24 @@ from django.conf import settings
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+class Presentation(models.Model):
+  title = models.CharField(max_length=100)
+  slug = models.SlugField(max_length=100)
+
+  def __str__(self):
+    return self.title
+
+
 class Poll(models.Model):
   question = models.CharField(max_length=254)
-  slug = models.SlugField(max_length=254)
   choices = ArrayField(models.CharField(max_length=254))
 
   published = models.BooleanField(default=False)
   ended = models.BooleanField(default=False)
 
-  next_poll = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-
   created = models.DateTimeField(auto_now_add=True)
+
+  presentation = models.ForeignKey(Presentation, on_delete=models.SET_NULL, blank=True, null=True)
 
   class Meta:
     ordering = ('-created',)
