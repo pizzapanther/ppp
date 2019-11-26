4198218b847790c5472b348ade6726a93970e050
Paul Bailey <paul.m.bailey@gmail.com>
2019-11-26 15:13:33 -0600

    csrf

--- ppp/settings/base.py
+++ ppp/settings/base.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
+    'ppp.middleware.DisableCSRF',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
